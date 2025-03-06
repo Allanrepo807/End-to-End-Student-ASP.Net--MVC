76e0d0197296767a6d5d8aa403b69558a08cd00c
Allan Benedict <allanben918@gmail.com>
2025-03-06 17:36:52 +0530

    Final changes have been made for this branch

--- Migrations/StudentContextModelSnapshot.cs
+++ Migrations/StudentContextModelSnapshot.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using WApp.Data;
+using WApp.Infrastructure.Data;
 
 #nullable disable
 
